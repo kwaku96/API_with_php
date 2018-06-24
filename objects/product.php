166feb3ee57186d515415d1f45ace241ca7b7b58
Baffour Kusi Frimpong <kwakukusi30@outlook.com>
2018-06-24 16:50:27 +0300

    second commit

--- objects/product.php
+++ objects/product.php
@@ -96,5 +96,60 @@ class Product{
         $this->category_id = $row['category_id'];
         $this->category_name = $row['category_name'];
     }
+
+    function update(){
+        $query = "UPDATE
+                " . $this->table_name . "
+            SET
+                name = :name,
+                price = :price,
+                description = :description,
+                category_id = :category_id
+            WHERE
+                id = :id";
+
+        $stmt = $this->conn->prepare($query);
+
+        // sanitize
+        $this->name=htmlspecialchars(strip_tags($this->name));
+        $this->price=htmlspecialchars(strip_tags($this->price));
+        $this->description=htmlspecialchars(strip_tags($this->description));
+        $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+        $this->id=htmlspecialchars(strip_tags($this->id));
+    
+        // bind new values
+        $stmt->bindParam(':name', $this->name);
+        $stmt->bindParam(':price', $this->price);
+        $stmt->bindParam(':description', $this->description);
+        $stmt->bindParam(':category_id', $this->category_id);
+        $stmt->bindParam(':id', $this->id);
+        
+        if($stmt->execute()){
+            return true;
+        }
+
+        return false;
+    }
+
+    function delete(){
+        // delete query
+        $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+    
+        // prepare query
+        $stmt = $this->conn->prepare($query);
+    
+        // sanitize
+        $this->id=htmlspecialchars(strip_tags($this->id));
+    
+        // bind id of record to delete
+        $stmt->bindParam(1, $this->id);
+    
+        // execute query
+        if($stmt->execute()){
+            return true;
+        }
+ 
+        return false;
+    }
 }
 ?>
--- product/delete.php
+++ product/delete.php
@@ -5,8 +5,8 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-// include database and object files
+ 
+// include database and object file
 include_once '../config/database.php';
 include_once '../objects/product.php';
 
@@ -15,26 +15,17 @@ $db = $database->getConnection();
 
 $product = new Product($db);
 
-//get id of the product to be edited from the url
 $data = json_decode(file_get_contents("php://input"));
 
 $product->id = $data->id;
-// set product property values
-$product->name = $data->name;
-$product->price = $data->price;
-$product->description = $data->description;
-$product->category_id = $data->category_id;
 
-if($product->update()){
+if($product->delete()){
     echo '{';
-        echo '"message": "Product was updated."';
+        echo '"message": "Product was deleted."';
     echo '}';
-}
- 
-// if unable to update the product, tell the user
-else{
+}else{
     echo '{';
-        echo '"message": "Unable to update product."';
+        echo '"message": "Unable to delete object."';
     echo '}';
 }
 ?>
